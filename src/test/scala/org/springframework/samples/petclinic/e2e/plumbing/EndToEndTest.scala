1d3ad59250ea80306b1bbb5b239f6cb76a110fa8
Jan Ouwens <jan.ouwens@gmail.com>
2017-09-20 16:45:58 +0200

    Simplifies 'never' function

--- src/test/scala/org/springframework/samples/petclinic/e2e/plumbing/EndToEndTest.scala
+++ src/test/scala/org/springframework/samples/petclinic/e2e/plumbing/EndToEndTest.scala
@@ -1,18 +1,9 @@
 package org.springframework.samples.petclinic.e2e.plumbing
 
-import org.scalactic.source.Position
 import org.scalatest._
 import org.scalatest.exceptions.TestFailedDueToTimeoutException
-import org.scalatest.time.{Millis, Seconds, Span}
-
-import scala.concurrent.duration._
 
 trait EndToEndTest extends FlatSpec with Matchers with ConfiguredEventually with OptionValues with GivenWhenThen {
-
-  /** Timeout configurations for our `never` function. */
-  private val neverPatienceConfig: PatienceConfig = patienceConfig
-  /** Timeout configurations for our `never` function. */
-  private val defaultAfterDelay: FiniteDuration = 5.second
 
   /**
     * Make a screenshot after every failing test.
@@ -34,11 +25,10 @@ trait EndToEndTest extends FlatSpec with Matchers with ConfiguredEventually with
     * Make sure that the given `fn` "never" occurs.
     * By "never", we mean "not within the specified timeouts".
     */
-  def never[T](fn: => T, delay: FiniteDuration = defaultAfterDelay, config: PatienceConfig = neverPatienceConfig)(implicit pos: Position): Assertion =
+  def never[T](fn: => T): Assertion =
     try {
-      Thread.sleep(delay.toMillis)
-      eventually(fn)(config, pos)
-      Assertions.fail("Condition is never completed successfully, which is not allowed")
+      eventually(fn)
+      Assertions.fail("Condition is completed successfully, which is not allowed")
     }
     catch {
       case _: TestFailedDueToTimeoutException => Assertions.succeed
